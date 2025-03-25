03b0689fe10c5bf1074a75350c2447bfd89df35e
kimjuyoung99 <jjassb404@naver.com>
2025-03-25 09:18:36 +0900

    이어붙힌수

--- PROGREMMERS/LEVEL_0/원소들의곱과합.js
+++ PROGREMMERS/LEVEL_0/원소들의곱과합.js
@@ -3,7 +3,5 @@ function solution(num_list) {
   const B = num_list.reduce((acc, curr) => acc + curr, 0) ** 2;
 
   return A > B ? 0 : 1;
-
-  return;
 }
 // https://school.programmers.co.kr/learn/courses/30/lessons/181929
