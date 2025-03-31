33b442e423ff785e8e1876a06ceb5122ce2c6da9
kimjuyoung99 <jjassb404@naver.com>
2025-03-31 20:00:00 +0900

    카운트 업

---
+++ PROGREMMERS/LEVEL_0/카운트업.js
@@ -0,0 +1,10 @@
+function solution(start_num, end_num) {
+  let answer = [];
+
+  for (let num = start_num; num <= end_num; num++) {
+    answer.push(num);
+  }
+
+  return answer;
+}
+// https://school.programmers.co.kr/learn/courses/30/lessons/181920
